bd2a5e7bca4c493768b51214964232a09fbafd7c
palassonde <pa.lassonde@gmail.com>
2016-07-13 14:10:03 -0400

    added hands

---
+++ public/js/game.js
@@ -0,0 +1,137 @@
+/* global Phaser */
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render })
+
+function preload () {
+    game.load.image('papier', 'assets/papier.png')
+    game.load.image('roche', 'assets/roche.png')
+    game.load.image('ciseau', 'assets/ciseau.png')
+}
+
+var socket
+var player
+var opponents
+var cursors
+
+function create () {
+
+    opponents = []
+    socket = io.connect()
+
+    // Resize our game world to be a 2000 x 2000 square
+    game.world.setBounds(-500, -500, 1000, 1000)
+
+    game.stage.backgroundColor = "#FFFFFF";
+
+    // The base of our player
+    player = game.add.sprite(0, 0, 'ciseau')
+    player.anchor.setTo(0.5, 0.5)
+
+    player.bringToTop()
+
+    cursors = game.input.keyboard.createCursorKeys()
+
+    // Start listening for events
+    setEventHandlers()
+}
+
+var setEventHandlers = function () {
+    // Socket connection successful
+    socket.on('connect', onSocketConnected)
+
+    // Socket disconnection
+    socket.on('disconnect', onSocketDisconnect)
+
+    // New player message received
+    socket.on('new player', onNewPlayer)
+
+    // Player move message received
+    socket.on('move player', onMovePlayer)
+
+    // Player removed message received
+    socket.on('remove player', onRemovePlayer)
+}
+
+// Socket connected
+function onSocketConnected () {
+    console.log('Connected to socket server')
+
+    opponents.forEach(function (opponent) {
+        opponent.player.kill()
+    })
+    opponents = []
+
+    // Send local player data to the game server
+    socket.emit('new player', { x: player.x, y: player.y })
+}
+
+// Socket disconnected
+function onSocketDisconnect () {
+    console.log('Disconnected from socket server')
+}
+
+// New player
+function onNewPlayer (data) {
+    console.log('New player connected:', data.id)
+
+    // Avoid possible duplicate players
+    var duplicate = playerById(data.id)
+    if (duplicate) {
+        console.log('Duplicate player!')
+        return
+    }
+
+    // Add new player to the remote players array
+    opponents.push(new RemotePlayer(data.id, game, player, data.x, data.y))
+}
+
+// Move player
+function onMovePlayer (data) {
+    var movePlayer = playerById(data.id)
+
+    // Player not found
+    if (!movePlayer) {
+        console.log('Player not found: ', data.id)
+        return
+    }
+
+    // Update player position
+    movePlayer.player.x = data.x
+    movePlayer.player.y = data.y
+}
+
+// Remove player
+function onRemovePlayer (data) {
+    var removePlayer = playerById(data.id)
+
+    // Player not found
+    if (!removePlayer) {
+        console.log('Player not found: ', data.id)
+        return
+    }
+
+    removePlayer.player.kill()
+
+    // Remove player from array
+    opponents.splice(opponents.indexOf(removePlayer), 1)
+}
+
+function update () {
+
+    socket.emit('move player', { x: player.x, y: player.y })
+}
+
+function render () {
+
+}
+
+// Find player by ID
+function playerById (id) {
+    for (var i = 0; i < opponents.length; i++) {
+        if (opponents[i].player.name === id) {
+            return opponents[i]
+        }
+    }
+
+    return false
+}
